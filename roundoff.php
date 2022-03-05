4c3cac381a3df6d44374428124980f4615209e8c
mariyamfidha <99707927+mariyamfidha@users.noreply.github.com>
2022-03-05 10:16:15 +0530

    Add files via upload

---
+++ roundoff.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+echo(round(0.60) . "<br>");
+echo(round(0.50) . "<br>");
+echo(round(0.49) . "<br>");
+echo(round(-4.40) . "<br>");
+echo(round(-4.60));
+?>
+</body>
+</html>
