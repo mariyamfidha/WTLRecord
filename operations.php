49c2c84ce0ccaf1298965a0d2e86161aacad3f8b
mariyamfidha <99707927+mariyamfidha@users.noreply.github.com>
2022-03-05 10:05:05 +0530

    Add files via upload

---
+++ operations.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+echo $x; 
+echo "<br>";
+echo $y;
+echo "<br>";
+echo "Sum:";
+echo $x + $y;
+echo "<br>";
+echo "Difference:";
+echo $x - $y;
+echo "<br>";
+echo "Product:";
+echo $x * $y;
+echo "<br>";
+echo "Quotient:";
+echo $x / $y;
+?>  
+
+</body>
+</html>
