b56b99a4aad2f08c9679e7a4863a931883c25a82
mariyamfidha <99707927+mariyamfidha@users.noreply.github.com>
2022-03-05 09:52:46 +0530

    Add files via upload

---
+++ minmax.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+echo "0, 150, 30, 20, -8, -200<br>";
+echo "Minimum is:";
+echo(min(0, 150, 30, 20, -8, -200) . "<br>");
+echo "Maximum is:";
+echo(max(0, 150, 30, 20, -8, -200));
+?>
+
+</body>
+</html>
